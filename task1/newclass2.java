d39b93b9000e6cd9e2b6364e247b10c37b252d85
tiwarishreyansh <131939283+tiwarishreyansh@users.noreply.github.com>
2025-06-18 12:25:12 +0530

    task1

---
+++ task1/newclass2.java
@@ -0,0 +1,21 @@
+
+class car{
+    String carname;
+    String model;
+    String color;
+    int torque;
+    boolean alloy;
+    boolean musicsystem;
+    int speed;
+    boolean fourbyfour;
+    boolean sunroof;
+}
+public class newclass2 {
+    public static void main(String[] args){
+        //object
+        Car vehical=new Car();
+        vehical.carname=polo;
+        vehical.torque=255;
+        vehical.sunroof=yes; 
+    }
+}
